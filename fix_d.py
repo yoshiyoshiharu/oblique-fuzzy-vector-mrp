5a1483be2c610deac70118b01eb4d21a7347a39e
yoshiyoshiharu <haruki1226yh@ezweb.ne.jp>
2021-11-28 18:33:12 +0900

    fix d

--- fix_d.py
+++ fix_d.py
@@ -1,18 +1,19 @@
 import numpy as np
 import p_n
 
-T = 4
+T = 5
 
 P = p_n.positive(p_n.A_inv(T))
 N = p_n.negative(p_n.A_inv(T))
 
-c = [1] * 8
+c = [1] * T * 2
 
 delta = np.array([
   [1000, 2000], 
   [500, 1500], 
-  [-1500, -500], 
-  [7500, 10000]
+  [-1000, -500], 
+  [-2000, -1000],
+  [8000, 10000]
 ])
 
 delta_l = delta[:, 0]
@@ -27,9 +28,7 @@ A = np.concatenate([P * (-1), N], axis=1)
 
 b = P @ delta_l + N @ delta_u
 
-bounds = [
-  (0, None),(0, None),(0, None),(0, None),(0, None),(0, None),(0, None),(0, None)
-]
+bounds = [(0, None)] * T * 2
 
 from scipy.optimize import linprog
 
